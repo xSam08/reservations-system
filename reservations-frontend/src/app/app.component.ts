9d150d0b66673d319c2f48c666e2a0d98d9f6240
Samuel Osuna <114777171+xSam08@users.noreply.github.com>
2025-05-14 07:38:40 -0500

    Set up Angular frontend structure

--- reservations-frontend/src/app/app.component.ts
+++ reservations-frontend/src/app/app.component.ts
@@ -1,12 +1,41 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+// Components
+import { MainComponent } from './Components/main/main.component';
+import { LoginComponent } from './Components/login/login.component';
+import { RegisterComponent } from './Components/register/register.component';
+import { HotelsComponent } from './Components/hotels/hotels.component';
+import { RoomsComponent } from './Components/rooms/rooms.component';
+import { ReservationComponent } from './Components/reservation/reservation.component';
+import { PaymentComponent } from './Components/payment/payment.component';
+import { ReviewsComponent } from './Components/reviews/reviews.component';
+import { ReportsComponent } from './Components/reports/reports.component';
+import { NotfoundComponent } from './Components/notfound/notfound.component';
+import { FooterComponent } from './Components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    MainComponent,
+    LoginComponent,
+    RegisterComponent,
+    HotelsComponent,
+    RoomsComponent,
+    ReservationComponent,
+    PaymentComponent,
+    ReviewsComponent,
+    ReportsComponent,
+    NotfoundComponent,
+    FooterComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'reservations-frontend';
+  title = 'Hotel Reservation System';
 }
